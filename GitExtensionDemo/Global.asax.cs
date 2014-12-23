1224a7e9735b5c9b29c5061cb1520cce35527c41
chevy <wangdai1990@qq.com>
2014-12-23 23:27:05 +0800

    l

--- GitExtensionDemo/Global.asax.cs
+++ GitExtensionDemo/Global.asax.cs
@@ -16,6 +16,7 @@ namespace GitExtensionDemo
     {
         protected void Application_Start()
         {
+
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
